f3f23d83811815541b4ad0ed3657d328f275ba77
Bora <nagayapim@gmail.com>
2024-08-14 07:50:07 +0300

    new release v1.0.1

--- session_cloner/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'getAllCookies') {
-      chrome.cookies.getAll({}, function(cookies) {
-        sendResponse({ cookies: cookies });
-      });
-    }
-    return true;
-  });
